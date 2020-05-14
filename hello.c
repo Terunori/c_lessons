f8e7012fda3f84ac5880323d033f5dc059f5b0ae
teru <18109251+Terunori@users.noreply.github.com>
2020-05-14 19:04:12 +0900

    lesson1-4

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Comments
+/*
+- STanDard IO
+- コメントは // でもよい
+- コンパイルは gcc hello.c -o hello.out (outputのo)
+*/
+
+int main(void) {
+    printf("hello world!¥n");
+    return 0;
+}
